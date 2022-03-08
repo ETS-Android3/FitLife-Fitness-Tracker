33f1e2e79714fd509dc84587b9e614b5501ee532
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-03-08 18:03:04 -0500

    Applied singelton design pattern to ensure only one instance of the database gets accessed for the user instead of multiple.

--- app/src/main/java/com/example/fitlife/MealWaterTracking.java
+++ app/src/main/java/com/example/fitlife/MealWaterTracking.java
@@ -59,7 +59,7 @@ public class MealWaterTracking extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate implements Dbconnection(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_water_tracking);
         new Dbconnection();

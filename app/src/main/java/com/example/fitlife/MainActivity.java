80d20c81fd933e9f112486e93784d0ca3ee98a79
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-03-24 16:00:21 -0400

    Setting up daily challenge to recur daily, now trying with alarm manager

--- app/src/main/java/com/example/fitlife/MainActivity.java
+++ app/src/main/java/com/example/fitlife/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.fitlife;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -57,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         userName = findViewById(R.id.userP);
 
-        handler.postDelayed(runnableCode, 86400000);
         handler.post(runnableCode);
+        handler.postDelayed(runnableCode, 86400000);
 
         //getDailyChallenge();
 
@@ -160,6 +163,23 @@ public class MainActivity extends AppCompatActivity {
         Timer timer = new Timer();
         timer.schedule(new setDailyChallenge(), getTomorrowMorning12AM(), 1000*60*60*24);
     }
+    public void scheduleAlarm() {
+        // Construct an intent that will execute the AlarmReceiver
+        Intent intent = new Intent(getApplicationContext(), MyAlarmReceiver.class);
+        // Create a PendingIntent to be triggered when the alarm goes off
+        final PendingIntent pIntent = PendingIntent.getBroadcast(this, MyAlarmReceiver.REQUEST_CODE,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Setup periodic alarm every every half hour from this point onwards
+        long firstMillis = System.currentTimeMillis(); // alarm is set right away
+        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
+        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
+                AlarmManager.INTERVAL_DAY, pIntent);
+    }
+
+
+
 
 
 

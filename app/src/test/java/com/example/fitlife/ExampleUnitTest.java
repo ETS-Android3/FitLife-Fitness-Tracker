7dca1b2132838f261906b1168caf24bbf95c8930
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-03-30 14:18:10 -0400

    Completed 5 unit tests for meal water tracking class

--- app/src/test/java/com/example/fitlife/ExampleUnitTest.java
+++ app/src/test/java/com/example/fitlife/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.example.fitlife;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,5 +14,49 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void WaterIntake_isCorrect()
+    {
+        int weight = 150;
+        int totalWater = weight/2;
+
+        assertEquals(totalWater, weight/2);
+    }
+    @Test
+    public void MaleBMR_isCorrect()
+    {
+        //double weight = 150;
+        //double height = 70;
+        //int age = 20;
+        //double BMR;
+
+
+        long actual = (long) (66 + (6.3 * 150) + (12.9 * 70) - (6.8 * 20));
+        assertEquals(1778, actual);
+
+    }
+    @Test
+    public void FemaleBMR_isCorrect()
+    {
+        //weight = 120, height = 60, age =20
+        long actual = (long) (655 + (4.3 * 120) + (4.7 * 60) - (4.7 * 20));
+        assertEquals(1359, actual);
+    }
+    @Test
+    public void ActivityLevel_isCorrect()
+    {
+        //No activity
+        long BMRwithactivity = (long) (1778*1.2);
+        assertEquals(2133, BMRwithactivity);
+    }
+    @Test
+    public void preferenceLevel_isCorrect()
+    {
+        //Cutting
+        long preference = (long)((1778*1.2)-250);
+        assertEquals(1883, preference);
+    }
+
+
 }
 //hello

0da370b5f2b660397a5ee6d062fd93b3cd247a52
Emil Melkic <melkicemil@gmail.com>
2022-04-10 19:22:12 -0400

    Added test values for the SearchFriends.java file.

--- app/src/test/java/com/example/fitlife/ExampleUnitTest.java
+++ app/src/test/java/com/example/fitlife/ExampleUnitTest.java
@@ -3,16 +3,25 @@ package com.example.fitlife;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import com.google.firebase.database.DataSnapshot;
+
+import androidx.annotation.Nullable;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest extends SearchFriends {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+    public void info_isCorrect() {
+        assertEquals("FirstName", "FirstName".toString());
     }
 }
-//hello
+
+// Checks if the user input for fist name contains only strings. This will allow us to avoid
+// anything that it is not a string input that may cause error for our app.
+// It will also make sure to convert it correctly to a string when a user logs on.
+// self-checking
+// timely

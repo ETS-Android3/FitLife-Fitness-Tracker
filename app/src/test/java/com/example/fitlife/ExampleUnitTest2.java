6ebcbd448fe95b952ff921af7eb8a65296e20a00
satluru <srija.atluru@gmail.com>
2022-04-11 22:47:42 -0400

    Create ExampleUnitTest2.java

---
+++ app/src/test/java/com/example/fitlife/ExampleUnitTest2.java
@@ -0,0 +1,18 @@
+package com.example.fitlife;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.google.firebase.database.DataSnapshot;
+
+import androidx.annotation.Nullable;
+
+
+public class exampleUnitTest2 extends LoginActivity  {
+    @Test
+
+    public void info_isCorrect() {
+        assertEquals("email", "email".toString());
+    }
+// checks to see if the email entered by the user is a string 

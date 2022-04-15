05da66646db8f015dba465d4c6337b27e189a117
Moamin1 <gw3810@wayne.edu>
2022-04-15 15:38:51 -0400

    Unit Test-Assignment 5

--- app/src/test/java/com/example/fitlife/ExampleUnitTest.java
+++ app/src/test/java/com/example/fitlife/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
+
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,5 +17,38 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void loginValidate(Object obj){
+        LoginActivity login = (LoginActivity) obj;
+        assertTrue(login.equals(2));
+    }
+    @Test
+    public void validateRegistration() {
+        sigginActivity user = new sigginActivity();
+        assertTrue(Boolean.parseBoolean(user.getString(199)));
+        assertNotNull(user.getString(100));
+    }
+    @Test
+    public void validateLeaderBoard() {
+        leaderBoard user = new leaderBoard();
+        assertTrue(user.equals(299));
+        assertNotNull(user.getString(1));
+    }
+
+
+    @Test
+    public void validatePhysicalActivity() {
+        PhysicalActivity user = new PhysicalActivity();
+        assertTrue(user.equals(20));
+    }
+
+    @Test
+    public void validateQuestionnaireActivty() {
+        qeustionaireActivity user = new qeustionaireActivity();
+        assertTrue(Boolean.parseBoolean(user.getString(1)));
+        assertEquals(193, 193);
+    }
 }
-//hello
+
+
+

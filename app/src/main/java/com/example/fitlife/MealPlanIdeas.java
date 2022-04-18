1d685a698ac0fe218aaa8b88c2c0a6ae66608086
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-04-17 22:30:30 -0400

    Added several meal options for users, with option to view on web

--- app/src/main/java/com/example/fitlife/MealPlanIdeas.java
+++ app/src/main/java/com/example/fitlife/MealPlanIdeas.java
@@ -1,14 +1,33 @@
 package com.example.fitlife;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MealPlanIdeas extends AppCompatActivity {
-
+Button meal, home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_plan_ideas);
-    }
+
+        meal = findViewById(R.id.btnTrack);
+        meal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MealWaterTracking.class));
+                finish();
+            }
+    });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
+}
 }

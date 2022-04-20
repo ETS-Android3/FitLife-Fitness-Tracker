f5606d10f0164f67719df3b0a8e35d509897618a
Tanvir-Rahman123 <gw4123@wayne.edu>
2022-04-19 22:59:17 -0400

    Updated meal plan ideas to onClickListener with Intent

--- app/src/main/java/com/example/fitlife/MealPlanIdeas.java
+++ app/src/main/java/com/example/fitlife/MealPlanIdeas.java
@@ -1,19 +1,102 @@
 package com.example.fitlife;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MealPlanIdeas extends AppCompatActivity {
-Button meal, home;
+    TextView oats, bagel, smoothie, chicken, salmon, pasta, steak, pizza, shake;
+    Button meal, home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_plan_ideas);
+        home = findViewById(R.id.homeBtn);
+        oats = findViewById(R.id.oats);
+        bagel = findViewById(R.id.bagel);
+        smoothie = findViewById(R.id.smoothie);
+        chicken = findViewById(R.id.chicken);
+        salmon = findViewById(R.id.salmon);
+        pasta = findViewById(R.id.pasta);
+        steak = findViewById(R.id.steak);
+        pizza = findViewById(R.id.pizza);
+        shake = findViewById(R.id.shake);
 
+
+        oats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://downshiftology.com/recipes/overnight-oats/")));
+                finish();
+            }
+        });
+        bagel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://themom100.com/recipe/breakfast-bagel-sandwich/")));
+                finish();
+            }
+        });
+        smoothie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://kristineskitchenblog.com/21-healthy-breakfast-smoothie-recipes/")));
+                finish();
+            }
+        });
+        chicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.campbells.com/recipes/one-dish-chicken-rice-bake/"
+)));
+                finish();
+            }
+        });
+        salmon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.skinnytaste.com/salmon-fried-rice/"
+)));
+                finish();
+            }
+        });
+        pasta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.foodandwine.com/pasta-noodles/pasta"
+)));
+                finish();
+            }
+        });
+        steak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.eatwell101.com/garlic-butter-steak-and-potatoes-recipe"
+)));
+                finish();
+            }
+        });
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://fithappyfoodie.com/2019/05/high-protein-low-calorie-pizza-400-cal-meal.html"
+)));
+                finish();
+            }
+        });
+        shake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.allrecipes.com/recipe/245163/the-best-post-workout-shake/"
+)));
+                finish();
+            }
+        });
         meal = findViewById(R.id.btnTrack);
         meal.setOnClickListener(new View.OnClickListener() {
             @Override

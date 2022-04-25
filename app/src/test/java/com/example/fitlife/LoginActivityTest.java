af15ad29d4e79ff07bfcadf8cd538edbcf4607e6
saifKhan10 <gw2131@wayne.edu>
2022-04-25 18:44:58 -0400

    pass test

--- app/src/test/java/com/example/fitlife/LoginActivityTest.java
+++ app/src/test/java/com/example/fitlife/LoginActivityTest.java
@@ -13,11 +13,6 @@ public class LoginActivityTest {
     }
 
     @Test
-    public void fail(){
-        assertEquals(3,1);
-    }
-
-    @Test
     public void emailNotNull(){
         testLoginActivityTest test = new testLoginActivityTest("fitlifecsc@gmail.com", "123456");
         assertEquals(true, test.emailNotNull());
